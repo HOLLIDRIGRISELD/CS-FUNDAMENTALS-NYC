f5483aa348623ba779747309c3620ded6c4ba701
HOLLIDRIGRISELD <hollidrigriseldi@gmail.com>
2025-01-27 02:08:36 +0200

    i was able to complete the week 8 ex2

--- Homework/week_08_b.py
+++ Homework/week_08_b.py
@@ -1,6 +1,10 @@
-#Use the Bored API https://bored-api.appbrewery.com/random and suggest a
+#Use the Bored API https://bored.api.lewagon.com/api/activity/  and suggest a
 #random activity for person who cannot decide what to do with their spare time. Extract and
 #display the “activity” element.
 
+import requests
 
-#the bored Api does not work at the time being so unfortunetly i cant complete the exercise.
+#get a random activity from the Bored API
+response = requests.get("https://bored.api.lewagon.com/api/activity/")
+data = response.json()
+print(f"Random Activity Suggestion: {data['activity']}")    
